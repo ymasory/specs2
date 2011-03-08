b3d6a0f2f71b5e9eea08087536fb97e0d09dca13
etorreborre <etorreborre@yahoo.com>
2011-03-08 17:52:18 +1100

    fixed the build for 2.8.0

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -5,6 +5,5 @@ class Plugins(info : ProjectInfo) extends PluginDefinition(info) {
    val snuggletex_repo = "snuggletex_repo" at "http://www2.ph.ed.ac.uk/maven2"
    val t_repo = "t_repo" at "http://tristanhunt.com:8081/content/groups/public/"
    
-   lazy val scctPlugin = "reaktor" % "sbt-scct-for-2.8" % "0.1-SNAPSHOT"
    lazy val posterous = "net.databinder" % "posterous-sbt" % "0.1.6"
 }
