8ffb75e7b694fef67974423c7197470d48fa74c9
etorreborre <etorreborre@yahoo.com>
2011-03-08 18:18:04 +1100

    fixed the drop method on CamelCase with 2.8.0

--- src/main/scala/org/specs2/text/CamelCase.scala
+++ src/main/scala/org/specs2/text/CamelCase.scala
@@ -9,9 +9,13 @@ trait CamelCase {
 
   implicit def camelCased(s: String) = new CamelCased(s)
   class CamelCased(s: String) {
-    def camelCaseToWords = s.drop(1).foldLeft(new StringBuilder(s.take(1).map(_.toLower))) { (res, cur) =>
-      res.append(if (cur.isUpper) (" " + cur.toLower) else cur)
-    }.toString
+    def camelCaseToWords = {
+	  if (s.length >= 1)
+	    s.drop(1).foldLeft(new StringBuilder(s.take(1).map(_.toLower))) { (res, cur) =>
+          res.append(if (cur.isUpper) (" " + cur.toLower) else cur)
+        }.toString
+	  else s
+	}
   }
 }
 
