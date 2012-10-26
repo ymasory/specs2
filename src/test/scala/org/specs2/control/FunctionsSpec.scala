0e84319416adb04a6c9f617d0743a43d11312923
etorreborre <etorreborre@yahoo.com>
2012-10-27 03:33:39 +1100

    trying out some new syntax for expectations and example groups

--- src/test/scala/org/specs2/control/FunctionsSpec.scala
+++ src/test/scala/org/specs2/control/FunctionsSpec.scala
@@ -3,18 +3,65 @@ package control
 
 import mutable.Specification
 import Functions._
+import execute._
 
 class FunctionsSpec extends Specification {
-  "a strict function can be transformed into a byname one" >> {
-    var result= 0
-    val f = (i: Int) => { result = 1 }
-    toByNameFunction1(f){ result = 2; 3 }
-    result === 1
-  }
+
   "a byname function can be transformed into a strict one" >> {
-    var result= 0
-    def f(i: =>Int) = 1
-    toStrictFunction1(f){ result = 2; 3 }
-    result === 2
+    def byNameFunction(u: =>Unit) {}
+    var parameter = "not evaluated"
+    toStrictFunction1(byNameFunction){ parameter = "evaluated" }
+
+    "The byname function has become a strict one" <==> (parameter === "evaluated")
   }
+
+  "functions can be or-ed with ||" >> {
+    val f1: String => Boolean = (_:String).length < 3
+    val f2: String => Boolean = (_:String).length < 5
+
+    (f1 || f2)("abcdefg") must beFalse
+    (f1 || f2)("abc")     must beTrue
+    (f1 || f2)("abcd")    must beTrue
+    (f2 || f1)("ab")      must beTrue
+  }
+  "functions can be and-ed with &&" >> {
+    val f1: String => Boolean = (_:String).length < 3
+    val f2: String => Boolean = (_:String).length < 5
+
+    (f1 && f2)("abcdefg") must beFalse
+    (f1 && f2)("abc")     must beFalse
+    (f1 && f2)("abcd")    must beFalse
+    (f2 && f1)("ab")      must beTrue
+  }
+  "functions can be negated with !" >> {
+    val f1: String => Boolean = (_:String).length < 3
+
+    (!f1)("abcdefg") must beTrue
+    (!f1)("ab")      must beFalse
+  }
+
+
+  implicit def isEquivalent(description: String): IsEquivalent = new IsEquivalent(description)
+  class IsEquivalent(description: String) {
+    def ==>[T : AsResult](result: =>T) = <==>(result)
+    def <==>[T : AsResult](result: =>T) = checkResultFailure {
+      val r = ResultExecution.execute(AsResult(result))
+      r match {
+        case i if i.isError || i.isFailure => i.mapMessage(m => negate(description)+" because "+m)
+        case other      => other.mapMessage(m => description+" <=> "+m)
+      }
+    }
+  }
+
+  def negate(description: String) =
+    Map(
+      "is"  -> "is not",
+      "has" -> "has not",
+      "have" -> "have not",
+      "must" -> "must not",
+      "should" -> "should not").
+      foldLeft(description) { case (res, (oldValue, newValue)) =>
+        res.replace(" "+oldValue+" ", " "+newValue+" ")
+      }
+
 }
--- src/test/scala/org/specs2/control/IncludeExcludeStackTraceFilterSpec.scala
+++ src/test/scala/org/specs2/control/IncludeExcludeStackTraceFilterSpec.scala
@@ -1,50 +1,131 @@
 package org.specs2
 package control
 import Throwablex._
+import matcher._
+import execute.{ResultExecution, Result, AsResult}
+import specification.{Outside, BeforeAfterAround, Before}
+import matcher.MatchPending
 
-class IncludeExcludeStackTraceFilterSpec extends Specification { def is =
+class IncludeExcludeStackTraceFilterSpec extends Specification with examples { def is =
 
   "A stacktrace can be filtered"                                                                                        ^
     "by declaring 'exclude' patterns"                                                                                   ^
-      "leaving the elements not matching the patterns"                                                                  ! e1^
-      "filtering out the elements matching the patterns"                                                                ! e2^
+      "leaving the elements not matching the patterns"                                                                  ! g1 .e1^
+      "filtering out the elements matching the patterns"                                                                ! g1 .e2^
                                                                                                                         p^
     "by declaring 'include' patterns"                                                                                   ^
-      "leaving the elements matching the patterns"                                                                      ! e3^
-      "filtering out the elements not matching the patterns"                                                            ! e4^
+      "leaving the elements matching the patterns"                                                                      ! g1 .e3^
+      "filtering out the elements not matching the patterns"                                                            ! g1 .e4^
                                                                                                                         endp^
   "A IncludedExcludedStackTraceFilter can be created from a string"                                                     ^
-    "the default pattern is i1,i2/e1,e2 where i are include tags and e are exclude tags"                                ! e5^
+    "the default pattern is i1,i2/e1,e2 where i are include tags and e are exclude tags"                                ! g2 .e1^
                                                                                                                         p^
   "From an existing IncludedExcludedStackTraceFilter"                                                                   ^
-    "we can add more include patterns, using the includeAlso method"                                                    ! e6^
-    "we can add more exclude patterns, using the excludeAlso method"                                                    ! e7^
+    "we can add more include patterns, using the includeAlso method"                                                    ! g3 .e1^
+    "we can add more exclude patterns, using the excludeAlso method"                                                    ! g3 .e2^
                                                                                                                         p^
   "A StackTraceFilter, when filtering an exception should"                                                              ^
-    "retain the exception cause"                                                                                        ! e8^
-    "retain the exception type"                                                                                         ! e9^
+    "retain the exception cause"                                                                                        ! g4 .e1^
+    "retain the exception type"                                                                                         ! g4 .e2^
                                                                                                                         end
+}
 
-  def stacktrace(st: String*) =  st.map(stackTraceElement(_))
+trait examples extends implementation with grouped {
 
-  def e1 = excludeTrace("t1", "t2").apply(stacktrace("t1", "a", "com.t1.other")).map(_.toString) must not containMatch("t1")
-  def e2 = excludeTrace("t1", "t2").apply(stacktrace("t1", "t3", "a", "com.t1.other")).map(_.toString) must containMatch("t3")
-
-  def e3 = includeTrace("t1", "t2").apply(stacktrace("t1", "a", "com.t1.other")).map(_.toString) must containMatch("t1")
-  def e4 = includeTrace("t1", "t2").apply(stacktrace("t1", "t3", "a", "com.t1.other")).map(_.toString) must not containMatch("t3")
-
-  def e5 = IncludeExcludeStackTraceFilter.fromString("i1,i2/e1,e2") must_== IncludeExcludeStackTraceFilter(Seq("i1", "i2"), Seq("e1", "e2"))
-
-  def e6 = DefaultStackTraceFilter.includeAlso("t1", "t2").apply(stacktrace("org.specs2", "t1")).map(_.toString) must not containMatch("specs2")
-  def e7 = DefaultStackTraceFilter.excludeAlso("t1").apply(stacktrace("org.specs2", "t1")).map(_.toString) must not containMatch("t1")
-
-  def e8 = {
-    val cause = new Exception("bang")
-    val e = new Exception("boom", cause)
-    DefaultStackTraceFilter.apply(e).getCause must_== cause
+  "patterns" - new g1 {
+    println("creating it")
+    e1 = filter(stacktrace("t1", "a", "com.t1.other"))      (excludeTrace("t1", "t2")) must not containMatch("t1")
+    e2 = filter(stacktrace("t1", "t3", "a", "com.t1.other"))(excludeTrace("t1", "t2")) must containMatch("t3")
+    e3 = filter(stacktrace("t1", "a", "com.t1.other"))      (includeTrace("t1", "t2")) must containMatch("t1")
+    e4 = filter(stacktrace("t1", "t3", "a", "com.t1.other"))(includeTrace("t1", "t2")) must not containMatch("t3")
   }
 
-  def e9 = DefaultStackTraceFilter.apply(new IllegalArgumentException("ohnoes")).getClass.getName ===
-           "java.lang.IllegalArgumentException"
+  "from string" - new g2 {
+    e1 = IncludeExcludeStackTraceFilter.fromString("i1,i2/e1,e2") must_==
+         IncludeExcludeStackTraceFilter(Seq("i1", "i2"), Seq("e1", "e2"))
+  }
 
+  "from an existing filter" - new g3 {
+    val defaultFilter = DefaultStackTraceFilter
+
+    e1 = filter(stacktrace("org.specs2", "t1"))(defaultFilter.includeAlso("t1", "t2")) must not containMatch("specs2")
+    e2 = filter(stacktrace("org.specs2", "t1"))(defaultFilter.excludeAlso("t1"))       must not containMatch("t1")
+  }
+
+  "exceptions" - new g4 {
+    e1 = {
+      val cause = new Exception("bang")
+      DefaultStackTraceFilter.apply(new Exception("boom", cause)).getCause must_== cause
+    }
+
+    e2 = DefaultStackTraceFilter.apply(new IllegalArgumentException("ohnoes")).getClass.getName ===
+        "java.lang.IllegalArgumentException"
+  }
+}
+
+trait groups { outer =>
+
+  trait g1 extends group {  }
+  trait g2 extends group {  }
+  trait g3 extends group {  }
+  trait g4 extends group {  }
+
+  var (g1, g2, g3, g4) = (() => new g1 {}: group,
+                          () => new g2 {}: group,
+                          () => new g3 {}: group,
+                          () => new g4 {}: group)
+
+  implicit def namedGroup(s: String): NamedGroup = new NamedGroup(s)
+  class NamedGroup(s: String) {
+    def -(g: =>group) =
+      if      (g.isInstanceOf[g1]) { g1 = () => g }
+      else if (g.isInstanceOf[g2]) { g2 = () => g }
+      else if (g.isInstanceOf[g3]) { g3 = () => g }
+      else if (g.isInstanceOf[g4]) { g4 = () => g }
+  }
+}
+
+trait grouped { outer =>
+
+  trait g1 extends group { outer.g1 = this }
+  trait g2 extends group { outer.g2 = this }
+  trait g3 extends group { outer.g3 = this }
+  trait g4 extends group { outer.g4 = this }
+
+  var (g1: group, g2: group, g3: group, g4: group) = (new g1 {}, new g2 {}, new g3 {}, new g4 {})
+
+  implicit def namedGroup(s: String): NamedGroup = new NamedGroup(s)
+  class NamedGroup(s: String) {
+    def -(g: group) = g
+  }
+}
+
+case class group(name: String = "") extends Expectations with Matchers with BeforeAfterAround {
+  private val pending = MatchPending(" -- PENDING", Expectable(()))
+  var e1: AnyAsResult = pending
+  var e2: AnyAsResult = pending
+  var e3: AnyAsResult = pending
+  var e4: AnyAsResult = pending
+
+  def before {}
+  def after {}
+  def around[T <% Result](a: =>T): Result = a
+
+  implicit def anyToAnyResult[T : AsResult](t: =>T): AnyAsResult = AnyAsResult(() => apply(AsResult(t)))
+}
+
+case class AnyAsResult(t: () => Result)
+
+object AnyAsResult {
+  implicit def anyResultAsResult[T]: AsResult[AnyAsResult] = new AsResult[AnyAsResult] {
+    def asResult(code: =>AnyAsResult): Result = code.t()
+  }
+}
+
+trait implementation extends Specification {
+  def stacktrace(st: String*) =  st.map(stackTraceElement(_))
+
+  /** filter a stacktrace */
+  def filter(stacktrace: Seq[StackTraceElement])(f: IncludeExcludeStackTraceFilter) =
+    f(stacktrace).map(_.toString)
 }
