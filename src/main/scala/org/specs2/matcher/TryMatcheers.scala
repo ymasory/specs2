74f2f9cc6b319ed564723acc7805e74014f6872f
Yuvi Masory <ymasory@gmail.com>
2013-02-08 16:00:37 -0800

    finish success cases

--- src/main/scala/org/specs2/matcher/TryMatcheers.scala
+++ src/main/scala/org/specs2/matcher/TryMatcheers.scala
@@ -15,6 +15,7 @@ private[specs2]
 trait TryBaseMatchers {
 
   /* Success[A] */
+  def beTSuccess[A] = new TSuccessMatcher[A]
   def beTSuccess[A](t: =>A) = new Matcher[Try[A]] {
     def apply[S <: Try[A]](value: Expectable[S]) = {
       val expected = t
@@ -24,51 +25,32 @@ trait TryBaseMatchers {
              value)
     }
   }
+
   def tsuccess[A](t: =>A) = beTSuccess(t)
-  def beTSuccess[A] = new TSuccessMatcher[A]
   def tsuccess[A] = beTSuccess[A]
 
   /* Failure */
-  // def beNone = new Matcher[Try[Any]] {
-  //   def apply[S <: Try[Any]](value: Expectable[S]) = {
-  //     result(value.value == None,
-  //            value.description + " is None",
-  //            value.description + " is not None",
-  //            value)
-  //   }
-  // }
-  // def none = beNone
-  // def beAsNoneAs[A](other: =>Try[A]) = new Matcher[Try[A]] {
-  //   def apply[S <: Try[A]](a: Expectable[S]) = {
-  //     val b = other
-  //     result(
-  //       a.value == None && b == None || a.value != None && b != None,
-  //       a.description + " is None as well",
-  //       if (a.value == None) b + " is not None" else a.description + " is not None",
-  //       a
-  //     )
-  //   }
-  // }
-  // def asNoneAs[A](other: =>Try[A]) = beAsNoneAs(other)
+
+  /* both Succeed or Fail */
 }
 private[specs2]
 trait TryBeHaveMatchers { outer: TryBaseMatchers =>
 
   implicit def toTryResultMatcher[A](result: MatchResult[Try[A]]) = new TryResultMatcher(result)
-  class TryResultMatcher[A](result: MatchResult[Try[A]]) {
-    // def beTSuccess = result(outer.beTSuccess)
-    // def beTSuccess(t: =>A) = result(outer.beTSuccess(t))
 
-    // def beNone = result(outer.beNone)
+  class TryResultMatcher[A](result: MatchResult[Try[A]]) {
+
+    def beTSuccess = result(outer.beTSuccess)
+    def beTSuccess(t: =>A) = result(outer.beTSuccess(t))
+
     def tsuccess = result(outer.beTSuccess)
     def tsuccess(t: =>A) = result(outer.beTSuccess(t))
-    // def none = result(outer.beNone)
-    // def asNoneAs(other: =>Try[A]) = result(beAsNoneAs(other))
   }
 }
 
 private[specs2]
 class TSuccessMatcher[A] extends Matcher[Try[A]] {
+
   def apply[S <: Try[A]](value: Expectable[S]) = {
     result(
       value.value.map(t => true).getOrElse(false),
@@ -77,7 +59,9 @@ class TSuccessMatcher[A] extends Matcher[Try[A]] {
       value
     )
   }
+
   def which(f: A => Boolean) = this ^^ { (t: Try[A]) => t filter f }
+
   def like(f: PartialFunction[A, MatchResult[_]]) = this and partialMatcher(f)
 
   private def partialMatcher(f: PartialFunction[A, MatchResult[_]]) = new Matcher[Try[A]] {
